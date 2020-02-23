5185d2c475ada910d55915ad82ed8dfcd34a9ce8
Lorblak <vasquez97548@gmail.com>
2020-02-23 20:31:56 -0300

    Se hicieron cambios en el main para procesar archivo; Problema de bucle infinito sin resolver

--- Final1/main.py
+++ Final1/main.py
@@ -1,5 +1,3 @@
-MAXIMO = '999999'
-
 def grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_maestro, descripcion):
 	linea_art_sin_vtas = cod_art_maestro + "," + descripcion
 	ar_art_sin_vtas.write(linea_art_sin_vtas)
@@ -19,41 +17,38 @@ def leer_linea(archivo, devolver):
 		linea = devolver
 	return linea.split(",")
 
-def cod_art_es_maximo(cod_art_maestro, cod_art_s1, cod_art_s2, cod_art_s3):
-	return cod_art_maestro != MAXIMO or cod_art_s1 != MAXIMO or cod_art_s2 != MAXIMO or cod_art_s3 != MAXIMO
-
 def actualizar_ventas(ar_articulos, ar_vtas_s1, ar_vtas_s2, ar_vtas_s3, ar_vtas_total, ar_art_sin_vtas, ar_art_erroneos):
-	cod_art_maestro, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
+	MAXIMO = '99999'
+	cod_art_mae, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
 	cod_art_s1, id_vta_s1, tot_vta_s1 = leer_linea(ar_vtas_s1, MAXIMO + ",,")
 	cod_art_s2, id_vta_s2, tot_vta_s2 = leer_linea(ar_vtas_s2, MAXIMO + ",,")
-	cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")
-	
-	tot_vtas_s1 = tot_vtas_s2 = tot_vtas_s3 = 0
-	while(cod_art_es_maximo(cod_art_maestro, cod_art_s1, cod_art_s2, cod_art_s3)):	
-		min_cod_art = min(cod_art_maestro, cod_art_s1, cod_art_s2, cod_art_s3)	
-		if cod_art_maestro not in [cod_art_s1, cod_art_s2, cod_art_s3]:
-			grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_maestro, descripcion)
-		"""	
-		while min_cod_art == cod_art_maestro:
-			grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_maestro, descripcion)
-			cod_art_maestro, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
-		"""
-		while cod_art_s1 == min_cod_art:
-			tot_vtas_s1 += int(tot_vta_s1)
-			cod_art_s1, id_vta_s1, tot_vta_s1 = leer_linea(ar_vtas_s1, MAXIMO + ",,")
-		while cod_art_s2 == min_cod_art:
-			tot_vtas_s2 += int(tot_vta_s2)
-			cod_art_s2, id_vta_s2, tot_vta_s2 = leer_linea(ar_vtas_s2, MAXIMO + ",,")
-		while cod_art_s3 == min_cod_art:
-			tot_vtas_s3 += int(tot_vta_s3)
-			cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")
-		grabar_art_vtas_total(ar_vtas_total, cod_art_maestro, str(tot_vtas_s1), str(tot_vtas_s2), str(tot_vtas_s3))
+	cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")	
+	while(cod_art_mae != MAXIMO or cod_art_s1 != MAXIMO or cod_art_s2 != MAXIMO or cod_art_s3 != MAXIMO):
+		acum_vtas_s1 = acum_vtas_s2 = acum_vtas_s3 = 0	
+		min_cod_art = min(cod_art_s1, cod_art_s2, cod_art_s3)
+		if cod_art_mae < min_cod_art:
+			grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_mae, descripcion)
+			cod_art_mae, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
+		elif cod_art_mae == min_cod_art:
+			while cod_art_s1 == min_cod_art:
+				acum_vtas_s1 += int(tot_vta_s1)
+				cod_art_s1, id_vta_s1, tot_vta_s1 = leer_linea(ar_vtas_s1, MAXIMO + ",,")
+			while cod_art_s2 == min_cod_art:
+				acum_vtas_s2 += int(tot_vta_s2)
+				cod_art_s2, id_vta_s2, tot_vta_s2 = leer_linea(ar_vtas_s2, MAXIMO + ",,")
+			while cod_art_s3 == min_cod_art:
+				acum_vtas_s3 += int(tot_vta_s3)
+				cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")
+			
+			grabar_art_vtas_total(ar_vtas_total, cod_art_mae, str(acum_vtas_s1), str(acum_vtas_s2), str(acum_vtas_s3))
 		cod_art_maestro, articulo = leer_linea(ar_articulos, MAXIMO + ",")		
+
 def main():
 	ar_articulos = open('articulos.csv', 'r')
 	ar_vtas_s1 = open('ventas_sucursal1.csv', 'r')
 	ar_vtas_s2 = open('ventas_sucursal2.csv', 'r')
 	ar_vtas_s3 = open('ventas_sucursal3.csv', 'r')
+	
 
 	ar_vtas_total = open('ventas_total.csv', 'w')
 	ar_art_sin_vtas = open('articulos_sin_ventas.csv', 'w')
