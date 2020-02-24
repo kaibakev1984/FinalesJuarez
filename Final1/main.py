b9554fc566f035ced9f7147c3c420a77298ff756
Lorblak <vasquez97548@gmail.com>
2020-02-24 20:22:12 -0300

    Se hizo una versión simplificada, trabajando con un solo archivo para verificar si la lógica del trabajo es la correcta; NOTA: make del funciona

--- Final1/main.py
+++ Final1/main.py
@@ -1,67 +1,58 @@
-def grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_maestro, descripcion):
-	linea_art_sin_vtas = cod_art_maestro + "," + descripcion
-	ar_art_sin_vtas.write(linea_art_sin_vtas)
-
-def grabar_art_vtas_total(ar_vtas_total, cod_art, tot_vtas_art_s1, tot_vtas_art_s2, tot_vtas_art_s3):
-	linea_vtas_total = cod_art + "," + tot_vtas_art_s1 + "," + tot_vtas_art_s2 + "," + tot_vtas_art_s3 + ","
-	ar_vtas_total.write(linea_vtas_total)
-
-def grabar_art_erroneos(ar_art_erroneos, cod_art_erroneo,  descripcion, mensaje_error):
-	linea_art_erroneo = cod_art_erroneo + "," + descripcion + "," + mensaje_error
-	ar_art_erroneos.write(linea_art_erroneo)
-
-def leer_linea(archivo, devolver):
+def leer_archivo(archivo, devolver):
 	linea = archivo.readline()
-	linea.rstrip("\n")
 	if not linea:
 		linea = devolver
-	return linea.split(",")
+	return linea.rstrip("\n").split(",")
 
-def actualizar_ventas(ar_articulos, ar_vtas_s1, ar_vtas_s2, ar_vtas_s3, ar_vtas_total, ar_art_sin_vtas, ar_art_erroneos):
-	MAXIMO = '99999'
-	cod_art_mae, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
-	cod_art_s1, id_vta_s1, tot_vta_s1 = leer_linea(ar_vtas_s1, MAXIMO + ",,")
-	cod_art_s2, id_vta_s2, tot_vta_s2 = leer_linea(ar_vtas_s2, MAXIMO + ",,")
-	cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")	
-	while(cod_art_mae != MAXIMO or cod_art_s1 != MAXIMO or cod_art_s2 != MAXIMO or cod_art_s3 != MAXIMO):
-		acum_vtas_s1 = acum_vtas_s2 = acum_vtas_s3 = 0	
-		min_cod_art = min(cod_art_s1, cod_art_s2, cod_art_s3)
-		if cod_art_mae < min_cod_art:
+def grabar_art_vtas_total(ar_vtas_total, cod_art, tot_vtas_art):
+		ar_vtas_total.write(cod_art + ',' + tot_vtas_art + '\n')
+
+def grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_sin_vtas, descripcion):
+	ar_art_sin_vtas.write(cod_art_sin_vtas + ',' + descripcion + '\n')
+
+def grabar_art_erroneos(ar_art_erroneos, cod_art_erroneo, descripcion_error):
+	ar_art_erroneos.write(cod_art_erroneo + ',' + descripcion_error)
+
+def calcular_ventas_totales(ar_articulos, ar_vtas_s1, ar_vtas_total, ar_art_sin_vtas, ar_art_erroneos):
+	MAXIMO = '9999'
+	cod_art_mae, descripcion = leer_archivo(ar_articulos, MAXIMO + ',')
+	cod_art_s1, id_vta_s1, tot_vta_s1 = leer_archivo(ar_vtas_s1, MAXIMO + ',,')
+	contador = 0	
+	while(cod_art_mae != MAXIMO or cod_art_s1 != MAXIMO):
+		if contador > 19:
+			return
+		contador += 1
+		tot_vtas_art = 0
+#		min_cod_art = min(cod_art_mae, cod_art_s1)
+		min_cod_art = cod_art_s1
+		if(cod_art_mae < cod_art_s1):
 			grabar_art_sin_vtas(ar_art_sin_vtas, cod_art_mae, descripcion)
-			cod_art_mae, descripcion = leer_linea(ar_articulos, MAXIMO + ",")
+			cod_art_mae, descripcion = leer_archivo(ar_articulos, MAXIMO + ',')
 		elif cod_art_mae == min_cod_art:
-			while cod_art_s1 == min_cod_art:
-				acum_vtas_s1 += int(tot_vta_s1)
-				cod_art_s1, id_vta_s1, tot_vta_s1 = leer_linea(ar_vtas_s1, MAXIMO + ",,")
-			while cod_art_s2 == min_cod_art:
-				acum_vtas_s2 += int(tot_vta_s2)
-				cod_art_s2, id_vta_s2, tot_vta_s2 = leer_linea(ar_vtas_s2, MAXIMO + ",,")
-			while cod_art_s3 == min_cod_art:
-				acum_vtas_s3 += int(tot_vta_s3)
-				cod_art_s3, id_vta_s3, tot_vta_s3 = leer_linea(ar_vtas_s3, MAXIMO + ",,")
-			
-			grabar_art_vtas_total(ar_vtas_total, cod_art_mae, str(acum_vtas_s1), str(acum_vtas_s2), str(acum_vtas_s3))
-		cod_art_maestro, articulo = leer_linea(ar_articulos, MAXIMO + ",")		
+			while min_cod_art == cod_art_s1:
+				tot_vtas_art += int(tot_vta_s1)
+				cod_art_s1, id_vta_s1, tot_vta_s1 = leer_archivo(ar_vtas_s1, MAXIMO + ',,')
+			grabar_art_vtas_total(ar_vtas_total, cod_art_mae, str(tot_vtas_art))
+		else:
+			if cod_art_mae > cod_art_s1:
+				grabar_art_erroneos(ar_art_erroneos, cod_art_s1, "El codigo de Articulo no existe")
+			cod_art_s1, id_vta_s1, tot_vta_s1 = leer_archivo(ar_vtas_s1, MAXIMO + ',,')
 
 def main():
+	print('Inicio Proceso')
 	ar_articulos = open('articulos.csv', 'r')
 	ar_vtas_s1 = open('ventas_sucursal1.csv', 'r')
-	ar_vtas_s2 = open('ventas_sucursal2.csv', 'r')
-	ar_vtas_s3 = open('ventas_sucursal3.csv', 'r')
 	
-
 	ar_vtas_total = open('ventas_total.csv', 'w')
 	ar_art_sin_vtas = open('articulos_sin_ventas.csv', 'w')
 	ar_art_erroneos = open('articulos_erroneos.txt', 'w')
-
-	actualizar_ventas(ar_articulos, ar_vtas_s1, ar_vtas_s2, ar_vtas_s3, ar_vtas_total, ar_art_sin_vtas, ar_art_erroneos)
+	
+	calcular_ventas_totales(ar_articulos, ar_vtas_s1, ar_vtas_total, ar_art_sin_vtas, ar_art_erroneos)
 	
 	ar_articulos.close()
 	ar_vtas_s1.close()
-	ar_vtas_s2.close()
-	ar_vtas_s3.close()
 	ar_vtas_total.close()
 	ar_art_sin_vtas.close()
 	ar_art_erroneos.close()
-
+	print('Fin Proceso')
 main()
